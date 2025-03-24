0c7baf3ace41a4ca499cc11ff85fba6b8eb7de95
Jacek Starz <jacek.starz@gmail.com>
2025-03-24 09:37:41 +0100

    Parse rom path from commandline arg

--- main.go
+++ main.go
@@ -1,66 +1,40 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
 	"os"
-	"time"
 
 	"github.com/faiface/pixel/pixelgl"
-	"github.com/gopxl/beep/v2"
-	"github.com/gopxl/beep/v2/mp3"
-	"github.com/gopxl/beep/v2/speaker"
 )
 
 func run() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <rom>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Provide a path to the ROM to load.\n")
+	}
+
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	vm := NewVM()
 
-	keymap := map[pixelgl.Button]uint8{
-		pixelgl.Key1: 0x1,
-		pixelgl.Key2: 0x2,
-		pixelgl.Key3: 0x3,
-		pixelgl.Key4: 0xC,
+	beeper := NewBeeper("assets/beep.mp3")
 
-		pixelgl.KeyQ: 0x4,
-		pixelgl.KeyW: 0x5,
-		pixelgl.KeyE: 0x6,
-		pixelgl.KeyR: 0xD,
-
-		pixelgl.KeyA: 0x7,
-		pixelgl.KeyS: 0x8,
-		pixelgl.KeyD: 0x9,
-		pixelgl.KeyF: 0xE,
-
-		pixelgl.KeyZ: 0xA,
-		pixelgl.KeyX: 0x0,
-		pixelgl.KeyC: 0xB,
-		pixelgl.KeyV: 0xF,
-	}
-
-	f, err := os.Open("assets/beep.mp3")
+	romPath := flag.Arg(0)
+	rom, err := os.ReadFile(romPath)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "File '%s' doesn't exist\n", romPath)
+		os.Exit(1)
 	}
 
-	streamer, format, err := mp3.Decode(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer streamer.Close()
-
-	buffer := beep.NewBuffer(format)
-	buffer.Append(streamer)
-
-	bufferSize := format.SampleRate.N(time.Second / 10)
-	speaker.Init(format.SampleRate, bufferSize)
-
-	// TODO: Load ROM from CLI arg
-	rom, err := os.ReadFile("roms/pong.ch8")
-	if err != nil {
-		panic(err)
-	}
 	vm.Load(rom)
 
+	keypad := NewKeypad()
 	disp, err := NewDisplay(&vm.GFX)
 	if err != nil {
 		panic(err)
@@ -68,7 +42,7 @@ func run() {
 
 	for range vm.cpuClock.C {
 		if !disp.win.Closed() {
-			for keyboardKey, pixelKey := range keymap {
+			for keyboardKey, pixelKey := range keypad.keys {
 				if disp.win.Pressed(pixelgl.Button(keyboardKey)) {
 					vm.KeyInputs[pixelKey] = 1
 				} else {
@@ -82,8 +56,7 @@ func run() {
 			}
 
 			if vm.beep {
-				sound := buffer.Streamer(0, buffer.Len())
-				speaker.Play(sound)
+				beeper.Beep()
 			}
 
 			if vm.draw {
